24da0ce2ec428259cf6e9df760f7991db40cf6dc
Salem-Abderaouf <salamasalem84@gmail.com>
2020-09-01 16:13:37 +0100

    get exact mouse position from canvas
    Signed-off-by: Salem-Abderaouf <salamasalem84@gmail.com>

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const canvas = document.getElementById("canva");
 const ctx = canvas.getContext("2d");
-const hexValue = document.getElementById('color-picker');
+const colorValue = document.getElementById('color-picker');
 const fontValue = document.getElementById('font-size');
 const clearbtn = document.getElementById('clear-btn')
 let defaultPenSize = 2;
 let defaultColor = "#000"
 let isClicked = false;
 
-console.log(canvas)
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect(),
+        scaleX = canvas.width / rect.width,
+        scaleY = canvas.height / rect.height;
+
+    return {
+        x: (evt.clientX - rect.left) * scaleX,
+        y: (evt.clientY - rect.top) * scaleY
+    }
+}
+
 clearbtn.addEventListener('click', () => { ctx.clearRect(0, 0, canvas.clientHeight, canvas.clientWidth); })
-hexValue.addEventListener('change', () => { defaultColor = hexValue.value })
+colorValue.addEventListener('change', () => { defaultColor = colorValue.value })
 fontValue.addEventListener('change', () => { defaultPenSize = fontValue.value })
 canvas.onclick = () => { isClicked = !isClicked; }
 canvas.addEventListener('mousemove', (e) => {
     if (isClicked) {
-        x = e.x;
-        y = e.y;
+        let pos = getMousePos(canvas, e);
         ctx.beginPath();
-        ctx.arc(x, y, defaultPenSize, 0, 2 * Math.PI);
+        ctx.arc(pos.x, pos.y, defaultPenSize, 0, 2 * Math.PI);
         ctx.fill();
         ctx.fillStyle = defaultColor;
     }
